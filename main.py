6b1a660f4b77b3a507450c0ca548797dc9255740
ddoavoca <ddoavoca@gmail.com>
2024-09-06 16:11:51 +0900

    2739	구구단

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 ###
-# 문제
-# 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
-
-# 입력
-# 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
-
-# 출력
-# 시험 성적을 출력한다.
+# 구구단 출력
 # ###
 
-while True:
-    score = int(input())
+NUM = int(input())
 
-    if(score//10 == 10):
-        print('A')
-    elif(score//10 == 9):
-        print('A')
-    elif(score//10 == 8):
-        print('B')    
-    elif(score//10 == 7):
-        print('C')
-    elif(score//10 == 6):
-        print('D')
-    elif(score//10 <= 5 ):
-        print('F')
+# 1
+# num = [1,2,3,4,5,6,7,8,9]
+# for number in num:
+#     print(NUM, '*', number,'=',NUM*num)
+
+# 2
+for number in range(1, 10):
+    print(NUM, '*', number,'=',NUM*number)
