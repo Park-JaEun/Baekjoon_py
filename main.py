fd49abe41effe4d28c509c6e02ec1420e0aebe19
ddoavoca <ddoavoca@gmail.com>
2024-09-06 21:32:41 +0900

    2675	문자열 반복

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 ###
 # 문제
-# 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
-
+# 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+# QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
 # 입력
-# 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
-
+# 첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다. 
 # 출력
-# 입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+# 각 테스트 케이스에 대해 P를 출력한다.
 ### 
 
-a=input()
-print(ord(a))
+
+
+n = int(input())
+s_list = [input().split() for _ in range(n)]
+
+for a in s_list:
+    # print(s_list)
+    n = list(a[1])
+    # print(n)
+
+    word = []
+
+    for num in n:
+        # print(num*int(a[0]))
+        word.append(num*int(a[0]))
+    
+    print("".join(word))
