ad925d17710766924743daa5d6ac8ee5427dd57a
ddoavoca <ddoavoca@gmail.com>
2024-09-07 01:23:12 +0900

    9020	골드바흐의 추측

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 ###
-# 문제
-# 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
-# QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
-# 입력
-# 첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다. 
-# 출력
-# 각 테스트 케이스에 대해 P를 출력한다.
 ### 
 
 def isPrime(NUM):
-    if NUM == 1:
-         return True
-
     for num in range(2, NUM):
-            if(NUM % num) == 0:
-                return True
-
-    return False
+        if(NUM % num) == 0:
+            return False
+    return True
 
 N = int(input())
+numbers = []
+for n in range(N):
+    numbers.append(int(input()))
 
-temp = map(int,input().split())
-for num in temp:
-    if isPrime(num) is True:
-        N -= 1
-
-print(N)
-
-
+for num in numbers:
+    for count in range(num//2):
+        if isPrime(num//2- count):
+           if isPrime(num - (num//2- count)):
+            print(num//2- count, num - (num//2- count))
+            break
