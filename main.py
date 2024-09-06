373574d4420583820417394632543c9a8602c82d
ddoavoca <ddoavoca@gmail.com>
2024-09-07 01:31:09 +0900

    10872	팩토리얼

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 ###
 ### 
 
-def isPrime(NUM):
-    for num in range(2, NUM):
-        if(NUM % num) == 0:
-            return False
-    return True
-
 N = int(input())
-numbers = []
-for n in range(N):
-    numbers.append(int(input()))
 
-for num in numbers:
-    for count in range(num//2):
-        if isPrime(num//2- count):
-           if isPrime(num - (num//2- count)):
-            print(num//2- count, num - (num//2- count))
-            break
+num = 1
+for n in range(1,N+1):
+    num *= n
+
+print(num)
