26ef0615a754db3417f1a72f76ef87f9a0814066
ddoavoca <ddoavoca@gmail.com>
2024-09-08 21:33:47 +0900

    1914	하노이 탑

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 ###
+# 하노이탑
 ### 
+
+def hanoi(N, start, end, temp):
+    if N == 1:
+        print(start, end)
+    else:
+        hanoi(N-1,start, temp, end)
+        print(start, end)
+        hanoi(N-1,temp, end, start)
+
 
 N = int(input())
 
-num = 1
-for n in range(1,N+1):
-    num *= n
+print(pow(2,N)-1)
 
-print(num)
+if N <= 20:
+    hanoi(N, 1,3,2)
+
+
+
+
+
