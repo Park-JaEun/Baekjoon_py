4a347ddab2c065b4ad8720bb1ba8f6d0001ad52b
ddoavoca <ddoavoca@gmail.com>
2024-09-09 15:27:00 +0900

    2750	수 정렬하기

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 ###
 # 문제
-# N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
-
-# N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
 
 # 입력
-# 첫째 줄에 N이 주어진다. (1 ≤ N < 15)
+# 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
 
 # 출력
-# 첫째 줄에 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력한다.
+# 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 ### 
 
 N = int(input())
 
-queen_cnt = 0
-queen_pos = [False] * N        # 열에 퀸이 있는지 여부
-dir41 = [False] * (2 * N) # 41대각선에 퀸이 있는지 여부
-dir63 = [False] * (2 * N) # 63대각선에 퀸이 있는지 여부
+numbers = []
 
-def solveNQueens(row):
-    global queen_cnt
-    if row == N:
-        queen_cnt += 1
-        return
-    
-    for col in range(N):
-        if not queen_pos[col] and not dir41[row + col] and not dir63[row - col + N]:
-        # 41대각선에 있는 좌표는 행+열의 값이 동일함
-        # 63대각선에 있는 좌표는 행-열의 값이 동일함
-        # 음수의 가능성이 있으니 N을 더해서 양수로 만듬
-            queen_pos[col] = dir41[row + col] = dir63[row - col + N] = True
-            solveNQueens(row + 1)
-            queen_pos[col] = dir41[row + col] = dir63[row - col + N] = False
+for i in range(N):
+    n = int(input())
+    numbers.append(n)
 
-solveNQueens(0)
-print(queen_cnt)
+numbers.sort()
+
+for i in range(N):
+    print(numbers[i])
+
 
 
 
